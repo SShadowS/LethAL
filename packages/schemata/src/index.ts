5768564929b0fe8463886df227fb92fb003b84c4
Torben Leth <SShadowS@SShadowS.dk>
2026-04-22 16:58:02 +0200

    feat(schemata): public API exports

--- packages/schemata/src/index.ts
+++ packages/schemata/src/index.ts
@@ -1,1 +1,18 @@
-export {};
+export { assignMutantIds } from "./ids";
+export type { IdedSpec } from "./ids";
+export { emitMutationSelector } from "./selector";
+export type { SelectorConfig } from "./selector";
+export { wrapStatement } from "./wrap";
+export type { WrapInput } from "./wrap";
+export { liftExpression } from "./lift";
+export type { LiftInput, LiftArtifacts } from "./lift";
+export { duplicateEnclosing } from "./duplicate";
+export type { DuplicateInput } from "./duplicate";
+export { compileSchemataForFile } from "./compile";
+export { writeInstrumentedProject } from "./project";
+export type {
+  InstrumentedFile,
+  WriteInput,
+  MutantManifest,
+  MutantManifestEntry,
+} from "./project";
