59a83f8d7c8af21134d4a9a5ff7490847a05f099
Torben Leth <SShadowS@SShadowS.dk>
2026-04-22 15:54:07 +0200

    feat(engine): public API exports

---
+++ packages/engine/src/index.ts
@@ -0,0 +1,43 @@
+// AST
+export { initParser, parseAL } from "./ast/parser";
+export { ALNodeKind, isALNodeKind } from "./ast/node-kinds";
+export { BINARY_EXPRESSION_KINDS, isBinaryExpressionKind } from "./ast/node-kinds";
+export type { ALSyntaxNode } from "./ast/syntax-node";
+export { wrapRoot, findFirst, findAll, visit } from "./ast/syntax-node";
+export { print, printWithRewrites } from "./ast/printer";
+export { astSubtreeHash } from "./ast/hash";
+export { canonicalize } from "./ast/canonicalization";
+export type { CanonicalForm } from "./ast/canonicalization";
+
+// Semantic
+export type {
+  SourceFile,
+  SymbolTable,
+  ObjectSymbol,
+  ProcedureSymbol,
+  VarSymbol,
+} from "./semantic/symbol-table";
+export { buildSymbolTable } from "./semantic/symbol-table";
+export type { CFG, BasicBlock } from "./semantic/cfg";
+export { buildCFG } from "./semantic/cfg";
+export type { TypeTable } from "./semantic/types";
+export { buildTypeTable } from "./semantic/types";
+export type { CallerIndex, CallSite } from "./semantic/callers";
+export { buildCallerIndex } from "./semantic/callers";
+export type { SemanticContext } from "./semantic/context";
+export { buildSemanticContext } from "./semantic/context";
+
+// Operator contract
+export type {
+  MutationOperator,
+  MutationSpec,
+  ConformanceCase,
+  ParentContextHint,
+  EquivalenceHint,
+  SemanticCapability,
+  AstNodeId,
+} from "./operator/interface";
+export { validateSpec } from "./operator/spec-validation";
+export type { ValidationResult } from "./operator/spec-validation";
+export { createRegistry } from "./operator/registry";
+export type { Registry } from "./operator/registry";
