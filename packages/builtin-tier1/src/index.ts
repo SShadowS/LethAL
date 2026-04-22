7a69aac3dfe42dbcdad82680e1361ecd980f32d8
Torben Leth <SShadowS@SShadowS.dk>
2026-04-22 18:09:27 +0200

    feat(builtin-tier1): public API exports + tier1Operators bundle

---
+++ packages/builtin-tier1/src/index.ts
@@ -0,0 +1,22 @@
+import type { MutationOperator } from "@lethal/operator-sdk";
+import { conditionalBoundary } from "./conditional-boundary";
+import { emptyBlock } from "./empty-block";
+import { negateConditional } from "./negate-conditional";
+import { returnValue } from "./return-value";
+import { voidMethodCall } from "./void-method-call";
+
+export { conditionalBoundary } from "./conditional-boundary";
+export { emptyBlock } from "./empty-block";
+export { negateConditional } from "./negate-conditional";
+export { returnValue } from "./return-value";
+export { voidMethodCall } from "./void-method-call";
+export { synthesizeAfter } from "./mutate-helpers";
+
+/** Convenience bundle for registering all Tier 1 operators at once. */
+export const tier1Operators: readonly MutationOperator[] = [
+  conditionalBoundary,
+  negateConditional,
+  voidMethodCall,
+  returnValue,
+  emptyBlock,
+];
